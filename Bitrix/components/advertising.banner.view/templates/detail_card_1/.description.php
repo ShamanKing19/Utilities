058260270af2c52288d10e59fc2f1d95165ab502
Сухомлин <g.sukhomlin@skillline.ru>
2022-12-12 16:49:36 +0200

    Битриксовый баннер

---
+++ Bitrix/components/advertising.banner.view/templates/detail_card_1/.description.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arTemplateDescription = [
+    "NAME" => 'detail_card_1',
+    "DESCRIPTION" => GetMessage("ADV_BS_DESCRIPTION")
+];
