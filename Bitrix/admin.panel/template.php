47e048790430cd0ec6be01ef10d06665fcfc5302
George <g.sukhomlin@skillline.ru>
2024-03-27 20:19:40 +0200

    ads

--- Bitrix/admin.panel/template.php
+++ Bitrix/admin.panel/template.php
@@ -3,47 +3,41 @@
 /* Добавление кнопок в панель админки */
 global $USER;
 // Проверка нужна, потому что у неавторизованного пользователя тоже появится окно админки
-if ($USER->IsAuthorized() && $USER->IsAdmin()) {
-    
-    // Переход к элементу
-    $APPLICATION->AddPanelButton(
-        [
-            'HREF' => '/bitrix/admin/' . CIBlock::GetAdminElementEditLink($iBlockId, $arResult['ID']),
-            'SRC' => "/bitrix/images/fileman/panel/web_form.gif",
-            'TEXT' => "Перейти к товару в админке",
-            'MAIN_SORT' => 400,
-            'TYPE' => 'SMALL',
-            'SORT' => 100
-        ]
-    );
+if ($USER->IsAdmin()) {
+    $iblockId = $arResult['IBLOCK_ID'];
+    $sectionId = $arResult['IBLOCK_SECTION_ID'];
 
-    // Переход к разделу
-    $APPLICATION->AddPanelButton(
-        [
-            'HREF' => '/bitrix/admin/' . CIBlock::GetAdminSectionEditLink($iBlockId, $sectionId),
+    $APPLICATION->AddPanelButton([
+        'HREF' => '/bitrix/admin/' . \CIBlock::GetAdminElementEditLink($iblockId, $arResult['ID']),
+        'SRC' => "/bitrix/images/fileman/panel/web_form.gif",
+        'TEXT' => 'Перейти к товару в админке',
+        'MAIN_SORT' => 400,
+        'TYPE' => 'SMALL',
+        'SORT' => 100
+    ]);
+
+    if ($sectionId) {
+        $APPLICATION->AddPanelButton([
+            'HREF' => '/bitrix/admin/' . \CIBlock::GetAdminSectionEditLink($iblockId, $sectionId),
             'SRC' => "/bitrix/images/fileman/panel/web_form.gif",
             'TEXT' => "Перейти к разделу товара в админке",
             'MAIN_SORT' => 400,
             'TYPE' => 'SMALL',
             'SORT' => 100
-        ]
-    );
+        ]);
+    }
 
+    $iblock = \Bitrix\Iblock\IblockTable::getRow(['filter' => ['=CODE' => $iblockCode], 'cache' => ['ttl' => 86400]]);
+    $typeId = $iblock[IBLOCK_TYPE_ID];
 
-    $iblock = \CIBlock::GetList([], ['CODE' => $iBlockCode])->fetch();
-    $iBlockId = $iblock['ID'];
-    $iblockType = $iblock['IBLOCK_TYPE_ID'];
-
-    $APPLICATION->AddPanelButton(
-        [
-            'HREF' => "/bitrix/admin/iblock_element_admin.php?IBLOCK_ID=$iblockId&type=$iblockType&lang=ru&find_section_section=$sectionId&SECTION_ID=$sectionId&apply_filter=Y",
-            'SRC' => '/bitrix/images/fileman/panel/web_form.gif',
-            'TEXT' => 'Перейти к инфоблоку',
-            'MAIN_SORT' => 400,
-            'TYPE' => 'SMALL',
-            'SORT' => 100
-        ]
-    );
+    $APPLICATION->AddPanelButton([
+        'HREF' => "/bitrix/admin/iblock_element_admin.php?IBLOCK_ID=$iblockId&type=$typeId&lang=ru&apply_filter=Y" . $sectionId ? "&find_section_section=$sectionId&SECTION_ID=$sectionId" : '',
+        'SRC' => '/bitrix/images/fileman/panel/web_form.gif',
+        'TEXT' => 'Перейти к инфоблоку',
+        'MAIN_SORT' => 400,
+        'TYPE' => 'SMALL',
+        'SORT' => 100
+    ]);
 }
 
 
